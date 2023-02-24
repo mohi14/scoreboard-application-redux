5d243eff7b56663971779864216d844c7399021f
mohi14 <mohiuddinkhan1409@gmail.com>
2023-02-24 22:58:27 +0600

    reset button done

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 
 const addNewMatchBtn = document.getElementById('addMatch')
+const resetMatchBtn = document.getElementById('resetMatch')
 const matchContainer = document.getElementById('matchContainer')
 
 const incrementInputField = document.getElementById('increment')
@@ -35,6 +36,12 @@ function scoreReducer(state = initialState, action) {
             }
         }
 
+    }
+    else if (action.type === 'reset') {
+        return {
+            ...state,
+            value: action.payload
+        }
     }
     else {
         state
@@ -85,26 +92,51 @@ addNewMatchBtn.addEventListener('click', () => {
 
     const newMatch = document.createElement('div')
     newMatch.classList.add('match')
-    newMatch.innerHTML = `<div class="wrapper">
+    newMatch.innerHTML = `   <div class="wrapper">
     <button class="lws-delete">
         <img src="./image/delete.svg" alt="" />
     </button>
     <h3 class="lws-matchName">Match ${matchCount}</h3>
 </div>
 <div class="inc-dec">
-    <div class="incrementForm">
+    <form class="incrementForm" id="increment">
         <h4>Increment</h4>
-        <input type="number" name="increment" class="lws-increment" />
-    </div>
-    <div class="decrementForm">
+        <input type="number" name="increment" class="lws-increment" id="incrementInput" />
+    </form>
+    <form class="decrementForm" id="decrement">
         <h4>Decrement</h4>
-        <input type="number" name="decrement" class="lws-decrement" />
-    </div>
+        <input type="number" name="decrement" class="lws-decrement" id="decrementInput" />
+    </form>
 </div>
 <div class="numbers">
-    <h2 class="lws-singleResult">0</h2>
+    <h2 class="lws-singleResult" id="result">0</h2>
 </div>`
 
     matchContainer.appendChild(newMatch)
-    console.log(5)
+
+
 })
+
+resetMatchBtn.addEventListener('click', () => {
+    store.dispatch({
+        type: "reset",
+        payload: 0
+    })
+})
+
+
+
+
+// allMatches.addEventListener('submit', function (event) {
+//     event.preventDefault()
+// })
+
+
+
+// for (const mat of allMatches) {
+//     mat.addEventListener('submit', function (event) {
+//         event.preventDefault()
+//     })
+
+//     console.log(mat)
+// }
